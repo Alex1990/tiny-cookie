8d8740986457f65613461a7adb5316eb652116be
Alex Chao <alexchao1990@gmail.com>
2018-04-26 07:38:32 +0800

    Add comment for secure test cases

--- test/secure-flag.spec.js
+++ test/secure-flag.spec.js
@@ -1,9 +1,12 @@
 describe('Cookie.set()', function() {
+  // Check the secure flag in browser devtools
+  // It should have the secure flag if you comment the next test case.
   it('should set secure when option is true', function() {
     Cookie.set('ssl_key', 'ssl_value', { secure: true });
     expect(Cookie.get('ssl_key')).to.equal('ssl_value');
   });
 
+  // It should have not the secure flag.
   it('should not set secure when option is false', function() {
     Cookie.set('ssl_key', 'ssl_value', { secure: false });
     expect(Cookie.get('ssl_key')).to.equal('ssl_value');
