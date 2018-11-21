48510ed0aba571039868b06c9342c22b48d345fa
Alex Chao <alexchao1990@gmail.com>
2018-11-21 22:23:33 +0800

    Declaring an ES module

--- flow/index.js
+++ flow/index.js
@@ -1,70 +1,73 @@
 // @flow
-type Decoder<T> = (value: string) => T;
-type Encoder<T> = (value: T) => string;
 
-declare type Cookies<T> = {
-  [name: string]: T
-}
+declare module 'tiny-cookie' {
+  type Decoder<T> = (value: string) => T;
+  type Encoder<T> = (value: T) => string;
 
-declare type CookieOptions = {
-  domain?: string,
-  path?: string,
-  expires?: string | number,
-  'max-age'?: number,
-  secure?: boolean,
-  samesite?: string,
-}
+  declare type Cookies<T> = {
+    [name: string]: T
+  }
 
-declare function isEnabled() : boolean;
+  declare type CookieOptions = {
+    domain?: string,
+    path?: string,
+    expires?: string | number,
+    'max-age'?: number,
+    secure?: boolean,
+    samesite?: string,
+  }
 
-/**
- * Get the cookie value by key.
- */
-declare function get<T>(key: string, decoder?: Decoder<T>) : T | null;
+  declare function isEnabled() : boolean;
 
-/**
- * Get all cookies
- */
-declare function getAll<T>(decoder?: Decoder<T>) : Cookies<T>;
+  /**
+   * Get the cookie value by key.
+   */
+  declare function get<T>(key: string, decoder?: Decoder<T>) : T | null;
 
-/**
- * Set a cookie.
- */
-declare function set<T>(key: string, value: T, encoder : Encoder<T>, options?: CookieOptions) : void;
+  /**
+   * Get all cookies
+   */
+  declare function getAll<T>(decoder?: Decoder<T>) : Cookies<T>;
 
-/**
- * Set a cookie.
- */
-declare function set(key: string, value: string, options?: CookieOptions) : void;
+  /**
+   * Set a cookie.
+   */
+  declare function set<T>(key: string, value: T, encoder : Encoder<T>, options?: CookieOptions) : void;
 
-/**
- * Remove a cookie by the specified key.
- */
-declare function remove(key: string, options?: CookieOptions) : void;
+  /**
+   * Set a cookie.
+   */
+  declare function set(key: string, value: string, options?: CookieOptions) : void;
 
-/**
- * Get the cookie's value without decoding.
- */
-declare function getRaw(key: string) : string | null;
+  /**
+   * Remove a cookie by the specified key.
+   */
+  declare function remove(key: string, options?: CookieOptions) : void;
 
-/**
- * Set a cookie without encoding the value.
- */
-declare function setRaw(key: string, value: string, options?: CookieOptions) : void;
+  /**
+   * Get the cookie's value without decoding.
+   */
+  declare function getRaw(key: string) : string | null;
 
-export {
-  isEnabled,
-  get,
-  getAll,
-  set,
-  getRaw,
-  setRaw,
-  remove,
-  isEnabled as isCookieEnabled,
-  get as getCookie,
-  getAll as getAllCookies,
-  set as setCookie,
-  getRaw as getRawCookie,
-  setRaw as setRawCookie,
-  remove as removeCookie
+  /**
+   * Set a cookie without encoding the value.
+   */
+  declare function setRaw(key: string, value: string, options?: CookieOptions) : void;
+
+  declare export {
+    isEnabled,
+    get,
+    getAll,
+    set,
+    getRaw,
+    setRaw,
+    remove,
+    isEnabled as isCookieEnabled,
+    get as getCookie,
+    getAll as getAllCookies,
+    set as setCookie,
+    getRaw as getRawCookie,
+    setRaw as setRawCookie,
+    remove as removeCookie
+  }
 }
