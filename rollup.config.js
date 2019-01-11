1b1e444d93fc3030ed741cb92798a91f3ae7b13c
Alex Chao <alexchao1990@gmail.com>
2019-01-11 08:00:28 +0800

    Upgrade dependencies: babel 6.x -> 7

--- rollup.config.js
+++ rollup.config.js
@@ -1,34 +1,31 @@
-import babel from 'rollup-plugin-babel'
-import babelrc from 'babelrc-rollup'
-import uglify from 'rollup-plugin-uglify'
+import babel from 'rollup-plugin-babel';
+import { uglify } from 'rollup-plugin-uglify';
 
-const env = process.env.NODE_ENV
+const env = process.env.NODE_ENV;
 
 const config = {
-  format: 'umd',
-  moduleName: 'Cookie',
+  input: 'src/index.js',
+  output: {
+    file: 'dist/tiny-cookie.js',
+    format: 'umd',
+    name: 'Cookie',
+  },
   plugins: [
-    babel(babelrc())
-  ]
-}
+    babel(),
+  ],
+};
 
 if (env === 'production') {
+  config.output.file = 'dist/tiny-cookie.min.js';
   config.plugins.push(
     uglify({
       compress: {
         pure_getters: true,
         unsafe: true,
         warnings: false,
-        screw_ie8: false
       },
-      mangle: {
-        screw_ie8: false
-      },
-      output: {
-        screw_ie8: false
-      }
-    })
-  )
+    }),
+  );
 }
 
-export default config
+export default config;
