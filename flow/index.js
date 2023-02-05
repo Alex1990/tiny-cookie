bf5d2cbd90eda2fa608e22cd3e66166d809a93c2
Alex Chao <alexchao1990@gmail.com>
2023-02-05 17:52:14 +0800

    The decoder cloud be null

--- flow/index.js
+++ flow/index.js
@@ -11,48 +11,48 @@ declare module 'tiny-cookie' {
   declare type CookieOptions = {
     domain?: string,
     path?: string,
-    expires?: string | number,
+    expires?: Date | string | number,
     'max-age'?: number,
     secure?: boolean,
     samesite?: string,
   }
 
-  declare function isEnabled() : boolean;
+  declare function isEnabled(): boolean;
 
   /**
    * Get the cookie value by key.
    */
-  declare function get<T>(key: string, decoder?: Decoder<T>) : T | null;
+  declare function get<T>(key: string, decoder?: Decoder<T> | null): T | null;
 
   /**
    * Get all cookies
    */
-  declare function getAll<T>(decoder?: Decoder<T>) : Cookies<T>;
+  declare function getAll<T>(decoder?: Decoder<T> | null): Cookies<T>;
 
   /**
    * Set a cookie.
    */
-  declare function set<T>(key: string, value: T, encoder : Encoder<T>, options?: CookieOptions) : void;
+  declare function set<T>(key: string, value: T, encoder: Encoder<T> | null, options?: CookieOptions): void;
 
   /**
    * Set a cookie.
    */
-  declare function set(key: string, value: string, options?: CookieOptions) : void;
+  declare function set(key: string, value: string, options?: CookieOptions): void;
 
   /**
    * Remove a cookie by the specified key.
    */
-  declare function remove(key: string, options?: CookieOptions) : void;
+  declare function remove(key: string, options?: CookieOptions): void;
 
   /**
    * Get the cookie's value without decoding.
    */
-  declare function getRaw(key: string) : string | null;
+  declare function getRaw(key: string): string | null;
 
   /**
    * Set a cookie without encoding the value.
    */
-  declare function setRaw(key: string, value: string, options?: CookieOptions) : void;
+  declare function setRaw(key: string, value: string, options?: CookieOptions): void;
 
   declare export {
     isEnabled,
